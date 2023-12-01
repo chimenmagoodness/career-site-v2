9c8183109716c3e091629d167e9e0913b46e43e9
chimenmagoodness <62130469+chimenmagoodness@users.noreply.github.com>
2023-12-01 05:10:34 +0000

    in progress

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return "<p> Hello World </p>"  #render_template('index.html')
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
